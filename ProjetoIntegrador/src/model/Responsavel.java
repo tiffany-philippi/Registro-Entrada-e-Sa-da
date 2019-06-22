a1cbe3628029e884bdc8782e6d38c50f1d2303f9
Tiffany Philippi <47127551+tiffany-philippi@users.noreply.github.com>
2019-06-22 19:37:11 -0300

    Excluir e consultar funcionando

--- ProjetoIntegrador/src/model/Responsavel.java
+++ ProjetoIntegrador/src/model/Responsavel.java
@@ -1,26 +1,40 @@
 package model;
 
-public class Responsavel extends Pessoa {
+public class Responsavel {
 	
 	private String cpfResp;
+	private String nomeResp;
+	private String enderecoResp;
 	private String telefone;
-	private String nome;
-	
-	public Responsavel(String cpfResp, String nome, String endereco, String telefoneResp, String email) {
-		super(nome, endereco, email);
-
-		this.cpfResp = cpfResp;
-		this.telefone = telefoneResp;
-	}
+	private String emailResp;
 
 	public String getCPFResp () {
 		return cpfResp;
 	}
+	public String getNomeResp () {
+		return nomeResp;
+	}
+	public void setNomeResp(String nomeResp) {
+		this.nomeResp = nomeResp;
+	}
+
+	public String getEnderecoResp () {
+		return enderecoResp;
+	}
+	public void setEnderecoResp(String enderecoResp) {
+		this.enderecoResp = enderecoResp;
+	}
 	public String getFoneResp () {
 		return telefone;
 	}
-	public void setNomeResp(String nome) {
-		this.nome = nome;
+	public void setFoneResp(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmailResp () {
+		return emailResp;
+	}
+	public void setEmailResp(String emailResp) {
+		this.emailResp = emailResp;
 	}
 	
 	
